81dd853465ac309052979c169af898ea3b432373
EdwardBelt <beltranmarper@gmail.com>
2026-03-02 21:57:03 +0100

    chore: update protocol constant to 774

--- core/src/main/java/es/edwardbelt/hycraft/protocol/ProtocolConstants.java
+++ core/src/main/java/es/edwardbelt/hycraft/protocol/ProtocolConstants.java
@@ -1,8 +1,8 @@
 package es.edwardbelt.hycraft.protocol;
 
 public final class ProtocolConstants {
-    public static final int PROTOCOL_VERSION = 773; // 1.21.10
-    public static final String MINECRAFT_VERSION = "1.21.10";
+    public static final int PROTOCOL_VERSION = 774; // 1.21.11
+    public static final String MINECRAFT_VERSION = "1.21.11";
     public static final int HYTALE_PROTOCOL_CRC = -1356075132;
     public static final int MAX_PACKET_SIZE = 2097152; // 2MB
     public static final int MAX_STRING_LENGTH = 32767;
